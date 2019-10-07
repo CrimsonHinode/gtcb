ed4f73f1d47863ddb6818415d4dfa48c2ca9c3c3
CrimsonHinode <56259015+CrimsonHinode@users.noreply.github.com>
2019-10-07 19:34:45 +0000

    Update guessing possibility

--- gtc1.py
+++ gtc1.py
@@ -2,18 +2,26 @@ import random
 
 colours = ["black", "white", "pink","gray","green","blue","yellow","red","purple","brown","orange"]
 target = random.choice(colours)
-    
+
+guessing = 0
+
 print ("Hello!")
 print ("Let's play a colour guess game.")
 print ("You can guess from these colours:")
 print ("black, white, pink, gray, green, blue, yellow, red, purple, brown, orange")
+print("You have 3 possibility.")
 
-while True:
+guessing = True
+while guessing < 4:
     print ("Take a guess.")
     guess = input()
+    guessing = guessing +1
     if guess == target:
         print ("Correct! You guessed it right!")
         break
     if guess != target:
         print ("Wrong!")
 
+else:
+    guessing = False
+    print("You lost! I was thinking: " + target)
