1e3b3cd75dfad9f57f4e244167fb2d1aa79dcb27
CrimsonHinode <56259015+CrimsonHinode@users.noreply.github.com>
2019-10-08 15:39:31 +0000

    Add delay for the instructions

--- gtc1.py
+++ gtc1.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 colours = ["black", "white", "pink","gray","green","blue","yellow","red","purple","brown","orange"]
 target = random.choice(colours)
@@ -6,10 +7,15 @@ target = random.choice(colours)
 guessing = 0
 
 print ("Hello!")
+time.sleep(1)
 print ("Let's play a colour guess game.")
+time.sleep(3)
 print ("You can guess from these colours:")
+time.sleep(3)
 print ("black, white, pink, gray, green, blue, yellow, red, purple, brown, orange")
+time.sleep(5)
 print("You have 3 possibility.")
+time.sleep(2)
 
 guessing = True
 while guessing < 4:
@@ -18,10 +24,13 @@ while guessing < 4:
     guessing = guessing +1
     if guess == target:
         print ("Correct! You guessed it right!")
+        time.sleep(2)
         break
     if guess != target:
         print ("Wrong!")
+        time.sleep(1)
 
 else:
     guessing = False
     print("You lost! I was thinking: " + target)
+    time.sleep(3)
