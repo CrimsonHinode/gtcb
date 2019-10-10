c92a99d7c8d8aeac2dd07c48f089ff1944789a18
CrimsonHinode <56259015+CrimsonHinode@users.noreply.github.com>
2019-10-10 16:33:44 +0000

    Add speech recognizer

--- gtc1.py
+++ gtc1.py
@@ -1,8 +1,11 @@
-import random
+import speech_recognition as sr
 import time
+import random
 
-colours = ["black", "white", "pink","gray","green","blue","yellow","red","purple","brown","orange"]
-target = random.choice(colours)
+
+colors = ["black", "white", "pink","grey","green","blue","yellow","red","purple","brown","orange"]
+target = random.choice(colors)
+r = sr.Recognizer()
 
 guessing = 0
 
@@ -12,25 +15,38 @@ print ("Let's play a colour guess game.")
 time.sleep(3)
 print ("You can guess from these colours:")
 time.sleep(3)
-print ("black, white, pink, gray, green, blue, yellow, red, purple, brown, orange")
+print ("black, white, pink, grey, green, blue, yellow, red, purple, brown, orange")
 time.sleep(5)
 print("You have 3 possibility.")
 time.sleep(2)
 
 guessing = True
 while guessing < 4:
-    print ("Take a guess.")
-    guess = input()
-    guessing = guessing +1
-    if guess == target:
-        print ("Correct! You guessed it right!")
-        time.sleep(2)
-        break
-    if guess != target:
-        print ("Wrong!")
-        time.sleep(1)
+    with sr.Microphone() as source:
+        print ("Take a guess.")
+        audio = r.listen(source)
+        guessing = guessing +1
+        try:
+            guess = r.recognize_google(audio)
+            print("You said: {}" .format(guess)) 
+            if guess == target:
+                print("Correct!")
+                guessing = str(guessing)     
+                print("You win!")
+                time.sleep(2)
+                break
+            if guess != target:
+                print("Wrong!")
+    
+        except BaseException as e:
+            print("Sorry, I didn't understand.")
+            guessing = guessing -1
 
+        
 else:
     guessing = False
+    target = str(target)  
     print("You lost! I was thinking: " + target)
     time.sleep(3)
+    
+
